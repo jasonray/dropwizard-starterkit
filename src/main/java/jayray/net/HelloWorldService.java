7f30aef7195b6e4b221af487f97cc19792fb1bd7
Jason Ray <jason.ray@agilex.com>
2014-01-24 16:02:58 -0500

    formatting

--- src/main/java/jayray/net/HelloWorldService.java
+++ src/main/java/jayray/net/HelloWorldService.java
@@ -8,24 +8,24 @@ import jayray.net.hello.EchoResource;
 import jayray.net.hello.HelloWorldResource;
 
 public class HelloWorldService extends Application<HelloWorldConfiguration> {
-	public static void main(String[] args) throws Exception {
-		new HelloWorldService().run(args);
-	}
+    public static void main(String[] args) throws Exception {
+        new HelloWorldService().run(args);
+    }
 
-	@Override
-	public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
-	}
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+    }
 
-	@Override
-	public void run(HelloWorldConfiguration configuration, Environment environment) {
-		final String template = configuration.getTemplate();
-		final String defaultName = configuration.getDefaultName();
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) {
+        final String template = configuration.getTemplate();
+        final String defaultName = configuration.getDefaultName();
 
-		environment.jersey().register(new HelloWorldResource(template, defaultName));
-		environment.jersey().register(new EchoResource());
-		environment.jersey().register(new CustomerResource());
+        environment.jersey().register(new HelloWorldResource(template, defaultName));
+        environment.jersey().register(new EchoResource());
+        environment.jersey().register(new CustomerResource());
 
-		environment.healthChecks().register("sample-healthcheck", new TemplateHealthCheck(template));
-	}
+        environment.healthChecks().register("sample-healthcheck", new TemplateHealthCheck(template));
+    }
 
 }
