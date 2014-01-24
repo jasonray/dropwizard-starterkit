97646f5145a7090779128b2a39291393333511ee
Jason Ray <jason.ray@agilex.com>
2014-01-24 16:06:28 -0500

    fix imports

--- src/main/java/jayray/net/hello/HelloWorldResource.java
+++ src/main/java/jayray/net/hello/HelloWorldResource.java
@@ -8,9 +8,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.codahale.metrics.annotation.Timed;
 import com.google.common.base.Optional;
 
